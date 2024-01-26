e92b01261ca0eb8781b14a8b319b6eb5683b6848
Lolomgrofl <luka.vuksanovic@hey.com>
2024-01-26 14:45:05 +0100

    Finished

--- exercises/00_intro/intro1.rs
+++ exercises/00_intro/intro1.rs
@@ -25,6 +25,7 @@ fn main() {
     println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
     println!(r#"                               |___/      "#);
     println!();
+    println!();
     println!("This exercise compiles successfully. The remaining exercises contain a compiler");
     println!("or logic error. The central concept behind Rustlings is to fix these errors and");
     println!("solve the exercises. Good luck!");
